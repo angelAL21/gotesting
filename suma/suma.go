9785ff8f76bd1f6d717c1b4594a8213ce5a765ba
unknown <angell_2104@hotmail.com>
2021-08-27 12:52:03 -0500

    we created the AddAcumulate func and his test

--- suma/suma.go
+++ suma/suma.go
@@ -3,3 +3,11 @@ package suma
 func Add(a, b int) int {
 	return a + b
 }
+
+func AddAcumulate(numbers ...int) int {
+	resp := 0
+	for _, v := range numbers {
+		resp += v
+	}
+	return resp
+}
--- suma/suma_test.go
+++ suma/suma_test.go
@@ -2,9 +2,22 @@ package suma
 
 import "testing"
 
+//name of Test
 func TestAdd(t *testing.T) {
-	want := 3
+	want := 4
 	got := Add(1, 3)
+	if got != want {
+		t.Logf("Error: se esperaba %d, se obtuvo %d", want, got)
+		t.Fail()
+	}
+}
+
+//name of test
+func TestAcumulate(t *testing.T) {
+	want := 7
+	//want := 7
+	got := AddAcumulate(1, 2, 3)
+
 	if got != want {
 		t.Logf("Error: se esperaba %d, se obtuvo %d", want, got)
 		t.Fail()
