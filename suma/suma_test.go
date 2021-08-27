913c9eacf95bcfe7b5e181645c3eca3214ca5139
unknown <angell_2104@hotmail.com>
2021-08-27 13:25:41 -0500

    we added logs in tests

--- suma/suma_test.go
+++ suma/suma_test.go
@@ -5,21 +5,24 @@ import "testing"
 //name of Test
 func TestAdd(t *testing.T) {
 	want := 4
+	t.Logf("want is %d", want)
 	got := Add(1, 3)
+	t.Logf("got is %d", got)
 	if got != want {
 		t.Logf("Error: se esperaba %d, se obtuvo %d", want, got)
 		t.Fail()
 	}
+	t.Log("terminó la prueba add")
 }
 
 //name of test
-func TestAcumulate(t *testing.T) {
-	want := 7
+func TestAddAcumulate(t *testing.T) {
+	want := 6
 	//want := 7
 	got := AddAcumulate(1, 2, 3)
 
 	if got != want {
-		t.Logf("Error: se esperaba %d, se obtuvo %d", want, got)
+		t.Logf("Error: se git pesperaba %d, se obtuvo %d", want, got)
 		t.Fail()
 	}
 }
