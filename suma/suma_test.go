fbf7938dd765a6c205b3fbcf8cba108427bce153
unknown <angell_2104@hotmail.com>
2021-08-27 13:38:24 -0500

    we compared the t.Logf with t.Fail with t.Errorf and when used them

--- suma/suma_test.go
+++ suma/suma_test.go
@@ -1,15 +1,18 @@
 package suma
 
-import "testing"
+import (
+	"testing"
+)
 
 //name of Test
 func TestAdd(t *testing.T) {
-	want := 4
+	want := 5
 	t.Logf("want is %d", want)
 	got := Add(1, 3)
 	t.Logf("got is %d", got)
 	if got != want {
 		t.Logf("Error: se esperaba %d, se obtuvo %d", want, got)
+		//t.Fail will fail when there is a error
 		t.Fail()
 	}
 	t.Log("terminó la prueba add")
@@ -18,11 +21,10 @@ func TestAdd(t *testing.T) {
 //name of test
 func TestAddAcumulate(t *testing.T) {
 	want := 6
-	//want := 7
 	got := AddAcumulate(1, 2, 3)
 
 	if got != want {
-		t.Logf("Error: se git pesperaba %d, se obtuvo %d", want, got)
-		t.Fail()
+		//combination of log and fail.
+		t.Errorf("Error: se pesperaba %d, se obtuvo %d", want, got)
 	}
 }
